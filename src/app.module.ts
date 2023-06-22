c5ba272309588eb9d8f40ad7c4101f9df8abfeb7
Fideles <fidelis.marcelo@gmail.com>
2023-06-22 19:08:34 +0000

    xpto123

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getEnvPath } from './common/helper/env.helper';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
   imports: [],
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,15 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  //getCarga(): double {
+  //}
+  }
+  //SELECT
+  // UNIDADE
+  // , ID_CARGA_TRANSPORTE
+  //  FROM
+  //  COMMAV.CMV_CARGA_EXPORTAR_RP_SAP
+  //  WHERE
+  //  DATA_CARGA = :Str_data_Carga
+  //  AND TRIP_NUMBER = :transporte_id;
 }
